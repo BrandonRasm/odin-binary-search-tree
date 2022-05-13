d0f58bfe2963c7c78b5f23c7be70870fff893606
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-05-12 21:08:04 -0700

    created Tree class with initialization method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -15,3 +15,14 @@ class Node
     @right = right
   end
 end
+
+# Pass in an array on initialization. Auto creates and balances a tree on initialize
+class Tree
+    def initialize(arr)
+        @root = build_tree(arr,0,arr.size - 1)
+    end
+
+    def build_tree(arr,first,last)
+        
+    end
+end
