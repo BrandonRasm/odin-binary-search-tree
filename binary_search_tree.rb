5f1e0505518e4e53d73a553db5a9f468067ea8c9
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-05-30 22:12:18 -0700

    added delete function and several supporting methods
    POSTMORTEM:
    My delete function(rightfully) looks like trash compared to other's
    solutions. With my unfamiliarity with binary search trees it didn't occur
    to me to swap a node at the bottom for a node at the top. I ended up
    recursivly going down the tree and shifting each node by 1(or 2 in an edge case).
    Because of this, my method is exponentailly more complicated than it
    needed to be and has about 1 billion checks for edge cases. Regardless,
    I'll keep it as is and learn better going forward.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -6,6 +6,8 @@ class Node
   attr_accessor :value, :left, :right
 
   def <=>(other)
+    return @value <=> other unless other.is_a?(Node)
+
     @value <=> other.value
   end
 
@@ -13,6 +15,25 @@ class Node
     @value = value
     @left = left
     @right = right
+  end
+
+  def right?(child)
+    child == @right
+  end
+
+  def child_count
+    counter = 0
+    counter += 1 unless @right.nil?
+    counter += 1 unless @left.nil?
+    counter
+  end
+
+  def only_child
+    return raise 'has 2 children' if child_count == 2
+    return raise 'has 0 childern' if child_count.zero?
+    return @left if @right.nil?
+
+    @right
   end
 end
 
@@ -51,16 +72,21 @@ class Tree
   end
 
   def delete(value)
-    current_node = @root
-    loop do
-      next_node = value > current_node.value ? current_node.right : current_node.left
-      return if next_node.nil?
-      break if next_node.value == value
+    pair = find_child_parent_pair(@root, value)
+    delete_node = pair[0]
+    parent = pair[1]
+    return delete_root_node if parent.nil?
 
-      current_node = next_node
-    end
+    # checks if node to be deleted has no child and dereferences and returns if so
+    return if delete_childless_node(delete_node, parent)
 
-    current_node.call(true) ? right : left
+    # bulk of work for delete function
+    new_child = move_up(delete_node, parent, true)
+
+    # checks if delete_node is already dereferenced in the tree
+    return if parent.right == new_child || parent.left == new_child
+
+    parent.right?(delete_node) ? parent.right = new_child : parent.left = new_child
   end
 
   def to_s
@@ -68,9 +94,31 @@ class Tree
     to_s_recursive(@root)
   end
 
-
-
   private
+
+  def delete_childless_node(delete_node, parent) # returns boolean weather it did anything
+    if delete_node.child_count.zero?
+      parent.right?(delete_node) ? parent.right = nil : parent.left = nil
+      return true
+    end
+    false
+  end
+
+  # returns arr containing found node(index 0) and its parent(index 1)
+  def find_child_parent_pair(current_node, value)
+    return [current_node, nil] if value == current_node.value # if deleted node is root node
+
+    next_node = nil
+    loop do
+      next_node = value > current_node.value ? current_node.right : current_node.left
+      return raise 'Value not found ' if next_node.nil?
+
+      break if next_node.value == value
+
+      current_node = next_node
+    end
+    [next_node, current_node]
+  end
 
   def prepare_array(user_array)
     user_array.uniq.sort
@@ -79,12 +127,69 @@ class Tree
   def to_s_recursive(current_node)
     puts current_node.left.value unless current_node.left.nil?
     to_s_recursive(current_node.left) unless current_node.left.nil?
+    puts '--'
     puts current_node.right.value unless current_node.right.nil?
     to_s_recursive(current_node.right) unless current_node.right.nil?
   end
+
+  # will return the child of node after modifying pointers
+  def move_up(node, parent, first)
+    case node.child_count
+    when 2
+      child = node.left
+      move_up(child, node, false)
+      child.right = node.right if child == node.left
+      child = node.left
+    when 1
+      move_up_with_1_child(node, parent, first)
+      # when 0:
+      # if no children child will be nil
+    end
+    child
+  end
+
+  def move_up_with_1_child(node, parent, first)
+    child = node.right.nil? ? node.left : node.right
+    return if parent.right?(node) == node.right?(child) # returns if node line is straight
+
+    # if node line is not straight
+    node.left = nil
+    node.right = nil
+    parent.right?(node) ? parent.right = child : parent.left = child
+    unless first # checks if move_up has been called at least once
+      insert_node_at(node, child) # if first==true node will be the value we are delete so we avoid inserting it
+      insert_node_at(parent.right, child) # uf first==true then parent.right will never risk being unassigned so inserting is pointless
+    end
+  end
+
+  def insert_node_at(node, node_location)
+    current_node = node_location
+    loop do
+      return raise 'Duplicate value' if node.value == current_node.value # probably unnecissary
+
+      next_node = node.value > current_node.value ? current_node.right : current_node.left
+      break if next_node.nil?
+
+      current_node = next_node
+    end
+
+    node.value > current_node.value ? current_node.right = node : current_node.left = node
+  end
+
+  # if child cound == 2 delete function will work normally
+  def delete_root_node
+    return @root = @root.only_child if @root.child_count == 1
+    return @root = nil if @root.child_count == 0
+
+    @root = move_up(@root,nil,true)
+  end
 end
 
-test = Tree.new([1, 2, 3, 4, 5, 6])
+test = Tree.new([4, 1, 44, 67, 6, 8, 99, 9, 16])
 test.to_s
-test.insert(13)
+puts '---------'
+test.insert(2)
+puts test.to_s
+puts '---------'
+test.delete(9)
 test.to_s
